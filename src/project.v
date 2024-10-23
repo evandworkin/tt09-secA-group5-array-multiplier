82ad8bc79a846514074fe6d5241f993c609d5835
Evan Dworkin <evanmdworkin@gmail.com>
2024-10-23 19:22:20 -0400

    Fixed project.v internal signals

--- src/project.v
+++ src/project.v
@@ -22,20 +22,20 @@ module tt_um_secA_group5_array_multiplier (
   wire [3:0] q = ui_in[3:0];
   wire [7:0] p;
   
-  wire [16:0] int;
+  wire [16:0] internal_wire;
   assign p[0] = m[0] & q[0];
-  full_adder fa00(.a(m[1] & q[0]), .b(m[0] & q[1]), .carry_in(1'b0), .sum(p[1]), .carry_out(int[0]));
-  full_adder fa01(a.(m[2] & q[0]), .b(m[1] & q[1]), .carry_in(int[0]), .sum(int[1]), .carry_out(int[2]));
-  full_adder fa02(.a(m[3] & q[0]), .b(m[2] & q[1]), .carry_in(int[2]), .sum(int[3]), .carry_out(int[4]));
-  full_adder fa03(.a(1'b0), .b(m[3] & q[1]), .carry_in(int[4]), .sum(int[5]), .carry_out(int[6]));
-  full_adder fa10(.a(m[0] & q[2]), .b(int[1]), .carry_in(1'b0), .sum(p[2]), .carry_out(int[7]));
-  full_adder fa11(.a(m[1] & q[2]), .b(int[3]), .carry_in(int[7]), .sum(int[8]), .carry_out(int[9]));
-  full_adder fa12(.a(m[2] & q[2]), .b(int[5]), .carry_in(int[9]), .sum(int[10]), .carry_out(int[11]));
-  full_adder fa13(.a(m[3] & q[2]), .b(int[6]), .carry_in(int[11]), .sum(int[12]), .carry_out(int[13]));
-  full_adder fa20(.a(m[0] & q[3]), .b(int[8]), .carry_in(1'b0), .sum(p[3]), .carry_out(int[14]));
-  full_adder fa21(.a(m[1] & q[3]), .b(int[14]), .carry_in(int[10]), .sum(p[4]), .carry_out(int[15]));
-  full_adder fa22(.a(m[2] & q[3]), .b(int[15]), .carry_in(int[12]), .sum(p[5]), .carry_out(int[16]));
-  full_adder fa23(.a(m[3] & q[3]), .b(int[16]), .carry_in(int[13]), .sum(p[6]), .carry_out(p[7]));
+  full_adder fa00(.a(m[1] & q[0]), .b(m[0] & q[1]), .carry_in(1'b0), .sum(p[1]), .carry_out(internal_wire[0]));
+  full_adder fa01(.a(m[2] & q[0]), .b(m[1] & q[1]), .carry_in(internal_wire[0]), .sum(internal_wire[1]), .carry_out(internal_wire[2]));
+  full_adder fa02(.a(m[3] & q[0]), .b(m[2] & q[1]), .carry_in(internal_wire[2]), .sum(internal_wire[3]), .carry_out(internal_wire[4]));
+  full_adder fa03(.a(1'b0), .b(m[3] & q[1]), .carry_in(internal_wire[4]), .sum(internal_wire[5]), .carry_out(internal_wire[6]));
+  full_adder fa10(.a(m[0] & q[2]), .b(internal_wire[1]), .carry_in(1'b0), .sum(p[2]), .carry_out(internal_wire[7]));
+  full_adder fa11(.a(m[1] & q[2]), .b(internal_wire[3]), .carry_in(internal_wire[7]), .sum(internal_wire[8]), .carry_out(internal_wire[9]));
+  full_adder fa12(.a(m[2] & q[2]), .b(internal_wire[5]), .carry_in(internal_wire[9]), .sum(internal_wire[10]), .carry_out(internal_wire[11]));
+  full_adder fa13(.a(m[3] & q[2]), .b(internal_wire[6]), .carry_in(internal_wire[11]), .sum(internal_wire[12]), .carry_out(internal_wire[13]));
+  full_adder fa20(.a(m[0] & q[3]), .b(internal_wire[8]), .carry_in(1'b0), .sum(p[3]), .carry_out(internal_wire[14]));
+  full_adder fa21(.a(m[1] & q[3]), .b(internal_wire[14]), .carry_in(internal_wire[10]), .sum(p[4]), .carry_out(internal_wire[15]));
+  full_adder fa22(.a(m[2] & q[3]), .b(internal_wire[15]), .carry_in(internal_wire[12]), .sum(p[5]), .carry_out(internal_wire[16]));
+  full_adder fa23(.a(m[3] & q[3]), .b(internal_wire[16]), .carry_in(internal_wire[13]), .sum(p[6]), .carry_out(p[7]));
 
   assign uo_out = p;
   assign uio_out = 0;
